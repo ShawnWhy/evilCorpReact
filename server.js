2d0be98e443183aec69f71994e2e35b8a2aa58d3
Shawn Yu <shawnyudesign@gmail.com>
2020-04-29 22:18:55 -0400

    sdsdsdS

--- server.js
+++ server.js
@@ -32,23 +32,22 @@ db.Employee.deleteMany({})
 ).catch(err=>console.log(err))
 
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-  app.use(express.static("build"));
+//   app.use(express.static("build"));
 
 
-  app.get("*", (req, res) => {
-       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-   });
+//   app.get("*", (req, res) => {
+//        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//    });
 
- }
+//  }
  app.use(express.static("build"));
 
 
  app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
   });
-
 
 
 

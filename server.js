f32154caa12753d634888150ffc8493322538068
Shawn Yu <shawnyudesign@gmail.com>
2020-04-29 20:22:37 -0400

    sdsd

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./public"));
-  app.use(express.static("./src"))
+  
 }
 // Add routes, both API and view
 app.use(routes);

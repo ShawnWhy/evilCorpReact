95d0ee66ac426d76a7ba1f0600ef90660d146f9f
Shawn Yu <shawnyudesign@gmail.com>
2020-04-29 20:16:56 -0400

    sdsd

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("./public"));
+  app.use(express.static("./src"))
 }
 // Add routes, both API and view
 app.use(routes);
